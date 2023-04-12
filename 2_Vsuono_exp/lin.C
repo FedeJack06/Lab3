d205f121c1fcf41abbeeb8f4859fcc1088c77b5d
FedeJack06 <giacobbefederico6@gmail.com>
2023-04-12 16:24:59 +0200

    add relazione 2, FIND COMMAND TO SHIFT FIT PANEL IN CANVAS

--- 2_Vsuono_exp/lin.C
+++ 2_Vsuono_exp/lin.C
@@ -8,22 +8,22 @@
       return;
    }
 
-   TGraphErrors gr;
+   TGraphErrors* gr = new TGraphErrors;
    int i = 0;
    double x, y, ex, ey;
    string uno, due, tre, quattro;
    inputfile >> uno >> due >> tre >> quattro;
    while (inputfile >> x >> y >> ex >> ey ) {
-      gr.SetPoint(i, x, y);
-      gr.SetPointError(i, 0.000001, 0.003);
+      gr->SetPoint(i, x, y);
+      gr->SetPointError(i, 0.000001, 0.003);
       i++;
    }
 
    
    // Calcolo dell'intervallo di definizione della funzione a partire dai
    // dati contenuti nel grafico
-   double xMin = TMath::MinElement(gr.GetN(), gr.GetX());
-   double xMax = TMath::MaxElement(gr.GetN(), gr.GetX());
+   double xMin = TMath::MinElement(gr->GetN(), gr->GetX());
+   double xMax = TMath::MaxElement(gr->GetN(), gr->GetX());
    double clearance = 0.1*(xMax - xMin);
    xMin -= clearance;
    xMax += clearance;
@@ -52,11 +52,27 @@
    gStyle->SetOptFit(1011);
    TCanvas c1("c1","canvas1",1920,1080);
    c1.cd();
-   gr.Draw("AP");
-   gr.SetTitle("distanza vs tempo percorrenza");
-   gr.GetXaxis()->SetTitle("tempo percorrenza [s]");
-   gr.GetYaxis()->SetTitle("distanza microfoni [m]");
+   gr->Draw("AP");
+   gr->SetTitle("distanza vs tempo percorrenza");
+   gr->GetXaxis()->SetTitle("tempo percorrenza [s]");
+   gr->GetYaxis()->SetTitle("distanza microfoni [m]");
 
    // Fit con la funzione f (che viene automaticamente graficata sui dati)
-   gr.Fit("f");
+   gr->Fit("f");
+   c1.Modified(); 
+   c1.Update();
+
+   TPaveStats* st = (TPaveStats*)gr->FindObject("stats");
+   st->SetX1NDC(0.15); 
+   st->SetX2NDC(0.55); 
+
+
+   //gr->GetListOfFunctions()->Clear(); // remove the "MC" box
+   //gr->SetStats(kFALSE); // prevent the "stats" box
+
+   c1.Modified(); 
+   c1.Update();
+
+
+   
 }
