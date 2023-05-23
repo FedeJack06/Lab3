3e33e51c63aec988ce5697c5754a5c2d8248b5e9
Andrea Cosso <andrebog11@gmail.com>
2023-05-23 23:18:45 +0200

    dati e macro, tutto funzionante

--- 3_Polarizzazione/attOtt.C
+++ 3_Polarizzazione/attOtt.C
@@ -12,20 +12,23 @@ void attOtt(){
     int i = 0;
     while (file >> conc >> angolo)
     {
-        gr->SetPoint(i, angolo, conc);
-        gr->SetPointError(i, 0, e);
+        gr->SetPoint(i, conc, angolo);
+        gr->SetPointError(i, 0.01*conc, 0.02);
         i++;
     }
 
-    auto f = new TF1("f","[0]+[1]*(x)",0.,0.);
+    auto f = new TF1("f","[0]+[1]*(x)",-0.1,0.6);
+    auto c = new TCanvas("c","c");
     f->SetParameters(0,1);
-
+    c->Draw();
+    c->DrawFrame(-0.1,-0.15,0.6,0.5);
     gStyle->SetOptFit(1011);
     gr->SetMarkerStyle(20);
-    gr->Draw("AP");
+    gr->Draw("P");
     gr->SetTitle("Concentrazione di glucosio vs angolo di rotazione polarizzazione");
     gr->GetXaxis()->SetTitle("angolo [rad]");
     gr->GetYaxis()->SetTitle("Concentrazione relativa []");
 
-    gr->Fit("f");
+    gr->Fit("f","R");
+    gr->Print();
 };
--- 3_Polarizzazione/malus.C
+++ 3_Polarizzazione/malus.C
@@ -1,6 +1,8 @@
 //Grafico e fit con cos^2
 //72  0.050   3e-4
 //77  0.049   3e-4
+
+//0.125 0.0451397
 void malus(){
 
     ifstream file("malus.dat");
@@ -12,13 +14,13 @@ void malus(){
     while (file >> theta >> Iout >> eI)
     {
         gr->SetPoint(i, (theta-52)*2*TMath::Pi()/100, Iout);
-        gr->SetPointError(i,e,eI);
+        gr->SetPointError(i,e/sqrt(3),eI);
         i++;
     }
 
-    auto f1 = new TF1("f1","[0]*pow(cos(x-[1]),2)",0.,0.);
-    //auto f1 = new TF1("f1","[0]*pow(cos(x-[1])+[2],2)+[3]",0.,0.);
-    //f1->SetParameters(0.3,0,0.1,0.01);
+    //auto f1 = new TF1("f1","[0]*pow(cos(x-[1]),2)",0.,0.);
+    auto f1 = new TF1("f1","[0]*pow(cos(x-[1]),2)+[3]",0.,0.);
+    f1->SetParameters(0.3,0.1,0.01);
     f1->SetParameters(0.3,0);
     //f1->SetParLimits(2,0,0.5);
     //f1->SetParLimits(0,2,0.3);
@@ -32,7 +34,7 @@ void malus(){
     gr->GetYaxis()->SetTitle("intensita in uscita []");
 
     gr->Fit("f1");
-    cout << "massimo della funzione: " << f1->GetMinimum(-0.2,3,1e-04) << endl;
+    cout << "massimo della funzione: " << f1->GetMaximumX(-0.6,0.6,1e-4) << endl;
     //gr->Print();
 };
 
